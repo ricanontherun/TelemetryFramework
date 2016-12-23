0232e592a6f475e1eee17a568fc4cf3627ab62b3
ricanontherun@gmail.com <ricanontherun@gmail.com>
2016-12-22 21:43:33 -0500

    Started fleshing out some data structures. Encapsulating zeromq interfaces within another class allows me to better handle exceptions.

--- client/main.cpp
+++ client/main.cpp
@@ -4,27 +4,86 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-  zmq::context_t context(1);
-  zmq::socket_t sender(context, ZMQ_REQ);
+class server
+{
+ private:
+  zmq::socket_t * socket;
+  zmq::context_t context;
+  const std::string addr;
 
-  sender.connect("tcp://localhost:5555");
+ public:
+  /**
+   * Construct a server object with an address.
+   *
+   * @param addr
+   */
+  server(const std::string & addr) :
+      context(zmq::context_t(1)),
+      socket(nullptr),
+      addr(addr) {}
 
-  zmq::message_t message(5);
-  memcpy(message.data(), "hello", 5);
+  bool connect() {
+    try {
 
-  while (true) {
-    std::cout << "Press enter to send the next request\n";
-    getchar();
+      this->socket = new zmq::socket_t(this->context, ZMQ_REQ);
+      this->socket->connect(this->addr);
 
-    std::cout << "Sending '" << (char *) message.data() << "' to server\n";
+    } catch (zmq::error_t & error) {
+      std::cerr << error.what() << "\n";
+      return false;
+    }
 
-    sender.send(message);
-
-    // Wait for the reply
-    std::cout << "Waiting for the json reply...\n";
-    zmq::message_t reply;
-    sender.recv(&reply);
-    std::cout << "Received something back from server!\n";
+    return true;
   }
+
+  bool communicate(const std::string & message_str, std::string & reply)
+  {
+    // this->send(this->make_message(message_str));
+
+    // this->receive(reply);
+    // return this->send(message_str)->receive(reply);
+    std::size_t message_length = message_str.length();
+
+    zmq::message_t message(message_length);
+
+    memcpy(message.data(), message_str.c_str(), message_length);
+
+    std::cout << (char *) message.data();
+    // Construct a message.
+    // Send it, block and wait for a reply.
+    // Write it to reply.
+  }
+
+  ~server() {
+    delete this->socket;
+  }
+};
+
+int main() {
+
+  server server("tcp://localhost:5555");
+  bool connect = server.connect();
+
+  std::string reply;
+  server.communicate("hey man", reply);
+
+  return 0;
+//
+//  zmq::message_t message(5);
+//  memcpy(message.data(), "hello", 5);
+//
+//  while (true) {
+//    std::cout << "Press enter to send the next request\n";
+//    getchar();
+//
+//    std::cout << "Sending '" << (char *) message.data() << "' to server\n";
+//
+//    sender.send(message);
+//
+//    // Wait for the reply
+//    std::cout << "Waiting for the json reply...\n";
+//    zmq::message_t reply;
+//    sender.recv(&reply);
+//    std::cout << "Received something back from server!\n";
+//  }
 }
